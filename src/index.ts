a8aa01b9716dd94e8e790cea2ad1696fe3d38c41
SaifAlqady <siafalqady51@gmail.com>
2023-06-20 12:27:35 +0300

    creating Docker files

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log('hello world')
+import express from 'express';
+import morgan from 'morgan';
+
+
+const app = express();
+
+app.use(morgan('dev'))
+
+app.get('/', (req, res) => {
+    res.json({'message':'Hello world'})
+})
+
+
+const port = Number(process.env.PORT || 8080)
+
+app.listen(port, '0.0.0.0', () => {
+    console.log(`listening on port ${port}`)
+})
+
